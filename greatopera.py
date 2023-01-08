d244548c7ab7bca7292ccbe322c6970d727056a9
Andrew Romond <aromond@Andrews-MacBook-Pro.local>
2023-01-07 23:55:47 -0500

    documentation is hard, if any of this is incorrect let me know :)

--- greatopera.py
+++ greatopera.py
@@ -1,14 +1,21 @@
 """
 Big thanks to bkobe for his assistance in deciphering the registers I needed to send to
 
-This script just takes one argument and sets one of 4 options 
+To use this script you will need the greatfet library installed:
+	
+	pip3 install greatfet
 
-the init argument initializes the operacake on first startup
+This script just takes one argument and sets one of 4 options. The init argument initializes the operacake on first startup
 
-A0<>A1 and B0<>B1
-A0<>A2 and B0<>B2
-A0<>A3 and B0<>B3
-A0<>A4 and B0<>B4
+1 sets A0<>A1 and B0<>B1
+2 sets A0<>A2 and B0<>B2
+3 sets A0<>A3 and B0<>B3
+4 sets A0<>A4 and B0<>B4
+
+example: 
+python3 greatopera.py init
+python3 greatopera.py 3
+
 
 The device Address is 0x18
 
@@ -20,6 +27,7 @@ The register values to set ports are comprised of 8 bits. Set the 8 bits you wan
 | gpio disable(1) enable(0) | A side 2 bits for 0-3 | A side 2 bits for 0-3 | B side 2 bits for 0-3 | B side 2 bits for 0-3 | Sameside(1) or Crossover(2) | LED Enable2 | LED Enable |
 +---------------------------+-----------------------+-----------------------+-----------------------+-----------------------+-----------------------------+-------------+------------+
 
+For example, setting our 8 bits to 10000111 would be port A1 and B1 connected to the same side and enableing LED's, converted to Hex this becomes 0x87 and is the value we need to set to the register 0x01
 
 """
 import sys
